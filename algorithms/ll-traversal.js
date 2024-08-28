e121fb6893e615740756b4194e4bc3ef1224459d
Revchenko Dmitry <revchenkofront@gmail.com>
2024-08-28 16:48:54 +0300

    implement linked list traversal

---
+++ algorithms/ll-traversal.js
@@ -0,0 +1,136 @@
+class LinkedListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+    shift() {
+        if (!this.head) {
+            return null;
+        }
+        const deletedHead = this.head;
+        if (this.head.next) {
+            this.head = this.head.next;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return deletedHead;
+    }
+    unshift(value) {
+        let newNode = new LinkedListNode(value, this.head);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+        this.head = newNode;
+        return this;
+    }
+    push(value) {
+        let newNode = new LinkedListNode(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        return this;
+    }
+    pop() {
+        if (!this.tail) {
+            return null;
+        }
+        let deletedTail = this.tail;
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+            return deletedTail;
+        }
+        let currentNode = this.head;
+        while (currentNode.next) {
+            if (!currentNode.next.next) {
+                currentNode.next = null;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        this.tail = currentNode;
+        return deletedTail;
+    }
+    deleteMany(value) {
+        if (!this.head) {
+            return null;
+        }
+        let deletedNode = null;
+        while (this.head && this.head.value === value) {
+            deletedNode = this.head;
+            this.head = this.head.next;
+        }
+        let currentNode = this.head;
+        if (currentNode !== null) {
+            while (currentNode.next) {
+                if (currentNode.next.value === value) {
+                    deletedNode = currentNode.next;
+                    currentNode.next = currentNode.next.next;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        if (this.tail && this.tail.value === value) {
+            this.tail = currentNode;
+        }
+
+        return deletedNode;
+    }
+    findFirst(value) {
+        if (!this.head) {
+            return null;
+        }
+        let currentNode = this.head;
+        while (this.head) {
+            if (currentNode.value === value) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+    toArray() {
+        let arr = [];
+        let currentNode = this.head;
+        while (currentNode) {
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return arr;
+    }
+}
+
+class TestLinkedList extends LinkedList {
+    straightTraversal() {
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+    reverseTraversal() {
+        this.reverseTraversalCB(this.head, console.log);
+    }
+    reverseTraversalCB(node, cb) {
+        if (node) {
+            this.reverseTraversalCB(node.next, cb);
+            cb(node.value);
+        }
+    }
+}
